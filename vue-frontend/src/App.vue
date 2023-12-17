18613893bcfc97cfc40731a500aa2b25461d147f
Micah Guttman <memtech3@gmail.com>
2023-12-17 02:22:46 -0600

    tailwind, trying to implement flowbite examples

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,33 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import {
-  IxBasicNavigation,
-  IxMenu,
-  IxMenuItem,
-  IxMenuSettings,
-  IxMenuSettingsItem
-} from '@siemens/ix-vue'
+import Navbar from './components/applicationFrame/navbar/Navbar.vue'
+
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
-  <IxBasicNavigation applicationName="Test" breakpoints="['sm','md']">
-    <div className="placeholder-logo" slot="logo"></div>
-    <IxMenu>
-      <RouterLink :to="{ name: 'home' }">
-        <IxMenuItem home-tab tab-icon="home"> Home </IxMenuItem>
-      </RouterLink>
-      <RouterLink :to="{ name: 'about' }">
-        <IxMenuItem tab-icon="about"> About </IxMenuItem>
-      </RouterLink>
-      <IxMenuSettings>
-        <IxMenuSettingsItem label="Example Setting 1"></IxMenuSettingsItem>
-        <IxMenuSettingsItem label="Example Setting 2"></IxMenuSettingsItem>
-      </IxMenuSettings>
-    </IxMenu>
-    <main>
-      <RouterView />
-    </main>
-  </IxBasicNavigation>
+  <Navbar />
+
 </template>
 
 <style scoped>
