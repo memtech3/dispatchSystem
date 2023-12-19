47214ce839b1a8c1acf5ae1a7440fa9812c85f51
Micah Guttman <memtech3@gmail.com>
2023-12-19 00:44:12 -0600

    storybook now

---
+++ vue-frontend/src/components/dispatching/CallsList.stories.js
@@ -0,0 +1,12 @@
+import CallsList from './CallsList.vue';
+
+export default {
+    component: CallsList,
+    title: 'Calls List',
+    tags: ['autodocs'],
+    argTypes: {},
+};
+
+export const Default = {
+    args: {},
+};
---
+++ vue-frontend/src/components/dispatching/UnitsList.stories.js
@@ -0,0 +1,12 @@
+import UnitsList from './UnitsList.vue';
+
+export default {
+    component: UnitsList,
+    title: 'Units List',
+    tags: ['autodocs'],
+    argTypes: {},
+};
+
+export const Default = {
+    args: {},
+};
