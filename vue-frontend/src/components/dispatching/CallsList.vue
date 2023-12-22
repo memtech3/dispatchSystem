057949c9a48ff03255375384bb8156472c7fb010
Micah Guttman <memtech3@gmail.com>
2023-12-22 14:42:47 -0600

    removed old and sample code

--- vue-frontend/src/components/dispatching/CallsList.vue
+++ vue-frontend/src/components/dispatching/CallsList.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { IxEventList, IxEventListItem } from '@siemens/ix-vue'
 import CallsListItem from './CallsListItem.vue'
 </script>
 
@@ -11,10 +10,6 @@ import CallsListItem from './CallsListItem.vue'
     <CallsListItem />
     <CallsListItem />
     <CallsListItem />
-    <!-- <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">Profile</li>
-    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Settings</li>
-    <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Messages</li>
-    <li class="w-full px-4 py-2 rounded-b-lg">Download</li> -->
   </ul>
 </template>
 
