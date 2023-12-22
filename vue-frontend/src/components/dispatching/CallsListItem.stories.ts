40f32c054b7b1b3bfb1f26dbff5f9685e97787f4
Micah Guttman <memtech3@gmail.com>
2023-12-22 14:11:45 -0600

    improvements to call list item design

--- vue-frontend/src/components/dispatching/CallsListItem.stories.ts
+++ vue-frontend/src/components/dispatching/CallsListItem.stories.ts
@@ -10,9 +10,12 @@ export const Default = {
   args: {
     callID: '1',
     callPriority: 1,
-    callTypeCode: '10-15',
-    callTypeDescription: 'Blegh',
-    callLocation: 'Hmmmmm',
-    callAssignedResources: 'ha none'
+    callPriorityClr: 'priorityColor-green',
+    callTypeCode: '2319',
+    callTypeDescription: 'Human Contamination',
+    callTypeIcon: 'bi-virus',
+    callLocation: 'Scare Floor Sector 7G',
+    callAssignedResources: 'P135, PS34, H43, H44',
+    callCreatedTime: new Date('2001-10-02T09:54:01.999Z')
   }
 }
--- vue-frontend/src/components/dispatching/CallsListItem.vue
+++ vue-frontend/src/components/dispatching/CallsListItem.vue
@@ -1,38 +1,61 @@
 <script setup lang="ts">
-import { IxEventListItem } from '@siemens/ix-vue'
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 
 defineProps<{
   callID: string
   callPriority: number
+  callPriorityClr: string
   callTypeCode: string
   callTypeDescription: string
+  callTypeIcon: string
   callLocation: string
   callAssignedResources: string // this needs to be an array
+  callCreatedTime: Date
 }>()
+
+// Attempt at call elapsed time counter:
+// 
+// data() {
+//   return {
+//     hours: 0,
+//     minutes: 0,
+//     seconds: 0
+//   };
+// };
+// mounted() {
+//   setInterval(() => this.);
+// };
+// methods: {
+//   updateTimer() {
+//     const currentDate = new Date();
+//     let elapsedDate = currentDate - this.callCreatedTime;
+//   }
+// }
 </script>
 
 <template>
-      <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-        </svg>
-        <span class="sr-only">Check icon</span>
-    </div>
   <li class="border border-gray-500" dark:border-gray-100>
     <div class="flex h-32">
-      <div class="w-20 text-green-400 bg-green-100 dark:bg-green-400 dark:text-green-100">
-        <div class="flex flex-col content-center items-center p-5 text-green-200">
-          <svg
-            class="w-6 h-6 text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 14"
+      <div class="w-14" :class="callPriorityClr">
+        <div class="flex flex-col content-center items-center py-5 text-green-200">
+          <div
+            class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200"
           >
-            <path
-              d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"
-            />
-          </svg>
+            <span :class="callTypeIcon"></span>
+            <!-- <svg
+              class="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+              />
+            </svg> -->
+            <span class="sr-only">Check icon</span>
+          </div>
           <span class="text-white"> P{{ callPriority }} </span>
         </div>
       </div>
@@ -42,12 +65,25 @@ defineProps<{
         {{ callID }} <br />
         {{ callAssignedResources }}
       </div>
-      <div class="w-40 bg-red-700"></div>
+      <div class="w-28 bg-red-700">test</div>
       <div class="w-8 bg-yellow-300"></div>
     </div>
   </li>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+@mixin cardPriorityColor($color) {
+  .priorityColor-#{$color} {
+    @apply text-#{$color}-400 bg-#{$color}-100 dark:bg-#{$color}-400 dark:text-#{$color}-100;
+  }
+
+}
+
+@include cardPriorityColor("orange");
+@include cardPriorityColor("green");
+@include cardPriorityColor("red");
+@include cardPriorityColor("purple");
+
 
 </style>
