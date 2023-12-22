b30120b30c138b93573497a9bf6742c60fd96982
Micah Guttman <memtech3@gmail.com>
2023-12-22 17:30:34 -0600

    take li element out of callListItem

--- vue-frontend/src/components/dispatching/CallsList.vue
+++ vue-frontend/src/components/dispatching/CallsList.vue
@@ -8,10 +8,66 @@ import CallsListItem from './CallsListItem.vue'
       <h3 class="font-semibold">Pending (5)</h3>
     </div>
     <ul class="space-y-2 text-sm font-medium text-gray-900 dark:text-white">
-      <CallsListItem />
-      <CallsListItem />
-      <CallsListItem />
-      <CallsListItem />
+      <li>
+        <CallsListItem
+          call-id="H345"
+          call-priority="1"
+          call-priority-clr="priorityColor-green"
+          call-type-code="2319"
+          call-type-description="Human Contamination"
+          call-type-icon="bi bi-virus"
+          call-location="Scare Floor Sector 7G"
+          call-assigned-resources="P135, PS34, H43, H44"
+        />
+      </li>
+      <li>
+        <CallsListItem
+          call-id="H345"
+          call-priority="1"
+          call-priority-clr="priorityColor-green"
+          call-type-code="2319"
+          call-type-description="Human Contamination"
+          call-type-icon="bi bi-virus"
+          call-location="Scare Floor Sector 7G"
+          call-assigned-resources="P135, PS34, H43, H44"
+        />
+      </li>
+      <li>
+        <CallsListItem
+          call-id="H345"
+          call-priority="1"
+          call-priority-clr="priorityColor-green"
+          call-type-code="2319"
+          call-type-description="Human Contamination"
+          call-type-icon="bi bi-virus"
+          call-location="Scare Floor Sector 7G"
+          call-assigned-resources="P135, PS34, H43, H44"
+        />
+      </li>
+      <li>
+        <CallsListItem
+          call-id="H345"
+          call-priority="1"
+          call-priority-clr="priorityColor-green"
+          call-type-code="2319"
+          call-type-description="Human Contamination"
+          call-type-icon="bi bi-virus"
+          call-location="Scare Floor Sector 7G"
+          call-assigned-resources="P135, PS34, H43, H44"
+        />
+      </li>
+      <li>
+        <CallsListItem
+          call-id="H345"
+          call-priority="1"
+          call-priority-clr="priorityColor-green"
+          call-type-code="2319"
+          call-type-description="Human Contamination"
+          call-type-icon="bi bi-virus"
+          call-location="Scare Floor Sector 7G"
+          call-assigned-resources="P135, PS34, H43, H44"
+        />
+      </li>
     </ul>
   </div>
 </template>
--- vue-frontend/src/components/dispatching/CallsListItem.vue
+++ vue-frontend/src/components/dispatching/CallsListItem.vue
@@ -34,54 +34,52 @@ defineProps<{
 </script>
 
 <template>
-  <li class="dark:text-white dark:bg-gray-800 rounded-md">
-    <div class="flex h-32">
-      <div
-        class="w-14 flex flex-col content-center items-center py-5 text-white"
-        :class="callPriorityClr"
+  <div class="flex h-32 dark:text-white dark:bg-gray-800 rounded-md">
+    <div
+      class="w-14 flex flex-col content-center items-center py-5 rounded-l-md text-white"
+      :class="callPriorityClr"
+    >
+      <span
+        class="inline-flex items-center justify-center w-10 h-10 text-2xl font-semibold text-gray-800 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-gray-300 mb-5"
       >
+        <span class="" :class="callTypeIcon"></span>
+        <span class="sr-only">{{ callTypeIcon }}</span>
+      </span>
+      <span class=""> P{{ callPriority }} </span>
+    </div>
+    <div class="p-2 grow min-w-96">
+      {{ callTypeCode }}: {{ callTypeDescription }} <br />
+      {{ callLocation }} <br />
+      {{ callID }} <br />
+      {{ callAssignedResources }}
+    </div>
+    <div class="w-28">
+      <div class="flex flex-col content-center items-end p-2">
+        <span>2:43 PM</span>
         <span
-          class="inline-flex items-center justify-center w-10 h-10 text-2xl font-semibold text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300 mb-5"
+          class="bg-gray-100 text-gray-800 text-sm font-medium px-1 py-0.05 rounded dark:bg-gray-700 dark:text-gray-300"
         >
-          <span class="" :class="callTypeIcon"></span>
-          <span class="sr-only">{{ callTypeIcon }}</span>
+          <span class="bi bi-stopwatch"></span>
+          10 min
         </span>
-        <span class=""> P{{ callPriority }} </span>
-      </div>
-      <div class="p-2 grow min-w-96">
-        {{ callTypeCode }}: {{ callTypeDescription }} <br />
-        {{ callLocation }} <br />
-        {{ callID }} <br />
-        {{ callAssignedResources }}
-      </div>
-      <div class="w-28">
-        <div class="flex flex-col content-center items-end p-2">
-          <span>2:43 PM</span>
-          <span
-            class="bg-gray-100 text-gray-800 text-sm font-medium px-1 py-0.05 rounded dark:bg-gray-700 dark:text-gray-300"
-          >
-            <span class="bi bi-stopwatch"></span>
-            10 min
-          </span>
-          <span>4 min</span>
-        </div>
-      </div>
-      <div class="w-8 flex flex-col items-center py-2 ms-3 me-1">
-        <button type="button" class="actionButton mb-8">
-          <span class="bi bi-three-dots-vertical"></span>
-        </button>
-        <button type="button" class="actionButton">
-          <span class="bi bi-chevron-double-down"></span>
-        </button>
+        <span>4 min</span>
       </div>
     </div>
-  </li>
+    <div class="w-8 flex flex-col items-center py-2 ms-3 me-1">
+      <button type="button" class="actionButton mb-8">
+        <span class="bi bi-three-dots-vertical"></span>
+      </button>
+      <button type="button" class="actionButton">
+        <span class="bi bi-chevron-double-down"></span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @mixin cardPriorityColor($color) {
   .priorityColor-#{$color} {
-    @apply bg-#{$color}-100 dark:bg-#{$color}-400 text-gray-700 font-bold;
+    @apply bg-#{$color}-100 dark:bg-#{$color}-400 text-gray-800 font-bold;
   }
 }
 
