7edd06d9d3572ea9a85e66c2a940dd465e2bfda8
Micah Guttman <memtech3@gmail.com>
2024-01-03 00:32:58 -0600

    formatter

--- vue-frontend/src/components/applicationFrame/sidebar/Sidebar.vue
+++ vue-frontend/src/components/applicationFrame/sidebar/Sidebar.vue
@@ -3,35 +3,33 @@ import SidebarItem from './SidebarItem.vue'
 </script>
 
 <template>
-  <ul
-    class="w-14 flex flex-col content-center items-center text-white bg-gray-800 flex-grow"
-  >
+  <ul class="w-14 flex flex-col content-center items-center text-white bg-gray-800 flex-grow">
     <li>
-      <SidebarItem state="active" icon="bi bi-collection"/>
+      <SidebarItem state="active" icon="bi bi-collection" />
     </li>
     <li>
-      <SidebarItem state="default" icon="bi bi-chat"/>
+      <SidebarItem state="default" icon="bi bi-chat" />
     </li>
     <li>
-      <SidebarItem state="default" icon="bi bi-telephone"/>
+      <SidebarItem state="default" icon="bi bi-telephone" />
     </li>
     <li>
-      <SidebarItem state="default" icon="bi bi-people"/>
+      <SidebarItem state="default" icon="bi bi-people" />
     </li>
     <li>
-      <SidebarItem state="disabled" icon="bi bi-person-check"/>
+      <SidebarItem state="disabled" icon="bi bi-person-check" />
     </li>
     <li>
-      <SidebarItem state="disabled" icon="bi bi-paperclip"/>
+      <SidebarItem state="disabled" icon="bi bi-paperclip" />
     </li>
     <li>
-      <SidebarItem state="disabled" icon="bi bi-sticky"/>
+      <SidebarItem state="disabled" icon="bi bi-sticky" />
     </li>
     <li>
       <SidebarItem state="disabled" icon="bi bi-clipboard" />
     </li>
     <li>
-      <SidebarItem state="disabled" icon="bi bi-geo-alt"/>
+      <SidebarItem state="disabled" icon="bi bi-geo-alt" />
     </li>
   </ul>
 </template>
