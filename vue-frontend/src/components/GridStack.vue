e9542a4344b5df2a9da227cb7748578b02b59f3e
Micah Guttman <memtech3@gmail.com>
2024-01-03 11:48:26 -0600

    gridstack example

---
+++ vue-frontend/src/components/GridStack.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import 'gridstack/dist/gridstack.min.css'
+import { GridStack } from 'gridstack'
+import { onMounted } from 'vue'
+
+let grid = null
+
+onMounted(() => {
+  grid = GridStack.init()
+  //   grid.el.appendChild(
+  //     '<div id="gsi-1" gs-x="0" gs-y="0" gs-w="3" gs-h="2" gs-auto-position="true"></div>'
+  //   )
+  grid.makeWidget('.grid-stack-item')
+})
+</script>
+
+<template>
+  <div class="grid-stack bg-white">
+    <div class="grid-stack-item bg-red-400">
+      <div class="grid-stack-item-content">Item 1</div>
+    </div>
+    <div class="grid-stack-item bg-green-400" gs-w="2">
+      <div class="grid-stack-item-content">Item 2</div>
+    </div>
+  </div>
+</template>
--- vue-frontend/src/views/AboutView.vue
+++ vue-frontend/src/views/AboutView.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import GridStack from '@/components/GridStack.vue'
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <GridStack />
 </template>
