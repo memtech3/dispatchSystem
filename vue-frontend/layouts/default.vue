1b41e6bb99c617c4b22f1642141c7c7a208c26ce
Micah Guttman <memtech3@gmail.com>
2024-01-15 17:00:03 -0600

    switched to grid layout for header/main/footer

--- vue-frontend/layouts/default.vue
+++ vue-frontend/layouts/default.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <ApplicationFrameNavbar class="fixed w-full top-0" />
-
-    <div class="flex-1 overflow-y-auto p-4">
-      <main>
-        <slot />
-      </main>
-    </div>
-
-    <ApplicationFrameFooter class="fixed w-full bottom-0" />
+  <div
+    class="grid grid-cols-1 grid-rows-[auto, 1fr, auto] gap-0 w-screen h-screen"
+  >
+    <ApplicationFrameNavbar class="self-start w-full" />
+    <main class="overflow-auto">
+      <slot />
+    </main>
+    <ApplicationFrameFooter class="self-end w-full" />
   </div>
 </template>
 <script lang="ts" setup></script>
