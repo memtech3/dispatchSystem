b3a33242254e14db39702af2bada67978ff40475
Micah Guttman <memtech3@gmail.com>
2024-01-27 03:35:01 -0600

    navbar can be smaller

--- vue-frontend/components/applicationFrame/Navbar.vue
+++ vue-frontend/components/applicationFrame/Navbar.vue
@@ -121,6 +121,6 @@
 
 <style scoped lang="scss">
 .navbar {
-  @apply bg-white border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 dark:border-gray-700;
+  @apply bg-white border-b border-gray-200 px-4 lg:px-6 py-2 dark:bg-gray-800 dark:border-gray-700;
 }
 </style>
