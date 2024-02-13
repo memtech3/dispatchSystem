4e7c1c6490c7e07098990b077ea841eed90f3277
Micah Guttman <memtech3@gmail.com>
2024-02-13 22:40:39 +0000

    got efcore working w/postrgesql, got hot chocolate working, got editor config setup, misc project setup

--- dotnet-backend/Program.cs
+++ dotnet-backend/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using dotnet_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+ConfigurationManager Configuration = builder.Configuration;
+
+builder.Services.AddGraphQLServer().RegisterDbContext<dotnet_backendContext>()
+.AddQueryType<Query>();
+builder.Services.AddDbContext<dotnet_backendContext>(options =>
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
 app.Run();
---
+++ dotnet-backend/Query.cs
@@ -0,0 +1,7 @@
+public class Query
+{
+    public string Hello(string name = "World")
+    {
+        return $"Hello, {name}!";
+    }
+}
