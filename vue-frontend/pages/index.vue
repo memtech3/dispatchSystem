08c39f9bd60f517223aa9a34b14320edd913bb00
Micah Guttman <memtech3@gmail.com>
2024-02-21 22:47:37 -0600

    copied over old call cards

--- vue-frontend/pages/index.vue
+++ vue-frontend/pages/index.vue
@@ -4,7 +4,7 @@
     class="grid grid-cols-[25%_1fr_1fr] h-full w-full divide-x divide-y divide-gray-200 dark:divide-gray-700"
   >
     <div class=""><EventForm /></div>
-    <div>2</div>
+    <div><CallsCardBoard /></div>
     <div
       class="h-full grid grid-rows-2 overflow-auto divide-x divide-y divide-gray-200 dark:divide-gray-700"
     >
