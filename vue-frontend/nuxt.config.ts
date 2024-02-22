2b40c75c9908560a59d07b4f8507d68e4e32c6a4
Micah Guttman <memtech3@gmail.com>
2024-02-21 22:47:46 -0600

    ran linter and formatter

--- vue-frontend/nuxt.config.ts
+++ vue-frontend/nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxt/ui', '@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
   ssr: false,
 })
