035269433efa50c0a587c37241e35c7d5ec4371d
Micah Guttman <memtech3@gmail.com>
2024-03-12 22:52:12 -0500

    removed extra fluff

--- vue-frontend/src/components/applicationFrame/Navbar.stories.ts
+++ vue-frontend/src/components/applicationFrame/Navbar.stories.ts
@@ -3,34 +3,11 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import Navbar from './Navbar.vue'
 
 const meta: Meta<typeof Navbar> = {
-  component: Navbar
+  component: Navbar,
+  tags: ['autodocs']
 }
 
 export default meta
 type Story = StoryObj<typeof Navbar>
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  render: () => ({
-    components: { Navbar },
-    template: '<Navbar background="#ff0" label="Navbar" />'
-  })
-}
-
-export const Secondary: Story = {
-  render: () => ({
-    components: { Navbar },
-    template: '<Navbar background="#ff0" label="😄👍😍💯" />'
-  })
-}
-
-export const Tertiary: Story = {
-  render: () => ({
-    components: { Navbar },
-    template: '<Navbar background="#ff0" label="📚📕📈🤓" />'
-  })
-}
+export const Primary: Story = {}
