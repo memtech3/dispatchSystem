3898760f40e7e881515a17e9b53f972a2aa4f8e1
Micah Guttman <memtech3@gmail.com>
2024-03-13 14:32:02 -0500

    added tailwind, tailwind works with storybook now

--- vue-frontend/.storybook/preview.ts
+++ vue-frontend/.storybook/preview.ts
@@ -4,6 +4,7 @@ import { setup } from '@storybook/vue3'
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/aura-dark-noir/theme.css'
 
+import '../src/tailwind.css'
 
 setup((app) => {
   app.use(PrimeVue)
---
+++ vue-frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+      }
+    },
+  },
+  plugins: [],
+}
+
