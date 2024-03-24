ed6ef9fc5e56722dbf32f96c25879a755fc2064e
Micah Guttman <memtech3@gmail.com>
2024-03-23 20:01:32 -0500

    nicer background color

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -3,7 +3,7 @@ import Navbar from './components/applicationFrame/Navbar.vue'
 import AppFooter from './components/applicationFrame/AppFooter.vue'
 </script>
 <template>
-  <div class="appGrid">
+  <div class="appGrid bg-slate-950">
     <Navbar class="mainNav" />
     <main class="overflow-auto">
       <RouterView />
