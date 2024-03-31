5ff31ed9e0bab02be1cdf81348639cba27dda4d1
Micah Guttman <memtech3@gmail.com>
2024-03-30 23:39:48 -0500

    start of rewrite of dispatchView with patternfly

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -3,28 +3,12 @@ import AppNavbar from './components/applicationFrame/AppNavbar.vue'
 import AppFooter from './components/applicationFrame/AppFooter.vue'
 </script>
 <template>
-  <div class="appGrid bg-slate-950 dark">
-    <AppNavbar class="mainNav" />
-    <main class="overflow-auto">
+  <div class="pf-v5-c-page dark">
+    <AppNavbar />
+    <main class="pf-v5-c-page__main">
       <RouterView />
     </main>
-    <AppFooter class="self-end w-full" />
+    <!-- <AppFooter class="self-end w-full" /> -->
   </div>
 </template>
 <script lang="ts" setup></script>
-
-<style>
-.appGrid {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto 1fr auto;
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-  height: 100vh;
-  width: 100vw;
-}
-.mainNav {
-  align-self: flex-start;
-  width: 100;
-}
-</style>
--- vue-frontend/src/views/DispatchView.vue
+++ vue-frontend/src/views/DispatchView.vue
@@ -1,28 +1,56 @@
 <script setup>
-import NewEventWindow from '@/components/NewEventWindow.vue'
 import EventForm from '@/components/EventForm/EventForm.vue'
 import MapComponent from '@/components/MapComponent.vue'
 import UnitsTable from '@/components/UnitsTable.vue'
 import CallsCardBoard from '@/components/calls/CallCardBoard.vue'
 </script>
 <template>
-  <!-- <NewEventWindow /> -->
-  <div
-    class="grid grid-cols-[25%_1fr_1fr] h-full w-full divide-x divide-y divide-gray-200 dark:divide-gray-700"
-  >
-    <div class="">
-      <EventForm />
-    </div>
-    <div><CallsCardBoard /></div>
-    <div
-      class="h-full grid grid-rows-2 overflow-auto divide-x divide-y divide-gray-200 dark:divide-gray-700"
-    >
-      <div class="">
-        <MapComponent />
+  <section class="pf-v5-c-page__main-section">
+    <div class="pf-v5-l-grid pf-m-gutter">
+      <div class="pf-v5-l-grid__item pf-m-gutter pf-m-3-col">
+        <div class="pf-v5-c-card" id="dashboard-demo-details-card-1">
+          <div class="pf-v5-c-card__title">
+            <h2 class="pf-v5-c-title pf-m-xl">Event Form</h2>
+          </div>
+          <div class="pf-v5-c-card__body">
+            <EventForm />
+          </div>
+          <hr class="pf-v5-c-divider" />
+          <div class="pf-v5-c-card__footer">
+            <a href="#">View Settings</a>
+          </div>
+        </div>
       </div>
-      <div>
-        <UnitsTable />
+      <div class="pf-v5-l-grid__item pf-m-gutter pf-m-4-col">
+        <div class="pf-v5-c-card" id="dashboard-demo-details-card-1">
+          <div class="pf-v5-c-card__title">
+            <h2 class="pf-v5-c-title pf-m-xl">Calls Board</h2>
+          </div>
+          <div class="pf-v5-c-card__body">
+            <CallsCardBoard />
+          </div>
+        </div>
+      </div>
+      <div class="pf-v5-l-grid__item pf-m-gutter pf-m-5-col">
+        <div class="pf-v5-l-flex pf-m-column pf-m-row-on-md pf-m-column-on-lg">
+          <div class="pf-v5-c-card" id="dashboard-demo-details-card-1">
+            <div class="pf-v5-c-card__title">
+              <h2 class="pf-v5-c-title pf-m-xl">Map</h2>
+            </div>
+            <div class="pf-v5-c-card__body">
+              <MapComponent />
+            </div>
+          </div>
+          <div class="pf-v5-c-card" id="dashboard-demo-details-card-1">
+            <div class="pf-v5-c-card__title">
+              <h2 class="pf-v5-c-title pf-m-xl">Units Board</h2>
+            </div>
+            <div class="pf-v5-c-card__body">
+              <UnitsTable />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
