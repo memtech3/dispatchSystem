ea2c02e7cd774dec0f1a3568f10db781a114a6cb
Micah Guttman <memtech3@gmail.com>
2024-04-01 17:57:32 -0500

    formatter

--- vue-frontend/src/components/EventForm/CommentsPanel.vue
+++ vue-frontend/src/components/EventForm/CommentsPanel.vue
@@ -4,7 +4,12 @@ import CommentItem from './CommentItem.vue'
 <template>
   <div>
     <h1>Comments</h1>
-    <pf-textarea resize-orientation="vertical" max-height="100" pattern="[0-9\s]+" placeholder="Numbers and spaces only" />
+    <pf-textarea
+      resize-orientation="vertical"
+      max-height="100"
+      pattern="[0-9\s]+"
+      placeholder="Numbers and spaces only"
+    />
     <ul class="p-2">
       <CommentItem comment="Additional report of mva, RP states 3 vehicles involved" />
       <CommentItem comment="Caller witnessed mva on highway 9" />
