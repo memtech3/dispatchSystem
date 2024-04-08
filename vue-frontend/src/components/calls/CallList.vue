4b3ed04e1fa35b11d47e9fc53530d08c3f72efa4
Micah Guttman <memtech3@gmail.com>
2024-04-07 19:23:35 -0500

    progress switching call cards to patternfly design system

--- vue-frontend/src/components/calls/CallList.vue
+++ vue-frontend/src/components/calls/CallList.vue
@@ -3,73 +3,61 @@ import CallListItem from './CallListItem.vue'
 </script>
 
 <template>
-  <div>
-    <div class="text-md text-gray-900 dark:text-white ms-2 mb-2">
-      <h3 class="font-semibold">Pending (5)</h3>
-    </div>
-    <ul class="space-y-2 text-sm font-medium text-gray-900 dark:text-white">
-      <li>
-        <CallListItem
-          call-id="H345"
-          call-priority="1"
-          call-priority-clr="priorityColor-green"
-          call-type-code="2319"
-          call-type-description="Human Contamination"
-          call-type-icon="bi bi-virus"
-          call-location="Scare Floor Sector 7G"
-          call-assigned-resources="P135, PS34, H43, H44"
-        />
-      </li>
-      <li>
-        <CallListItem
-          call-id="H345"
-          call-priority="1"
-          call-priority-clr="priorityColor-green"
-          call-type-code="2319"
-          call-type-description="Human Contamination"
-          call-type-icon="bi bi-virus"
-          call-location="Scare Floor Sector 7G"
-          call-assigned-resources="P135, PS34, H43, H44"
-        />
-      </li>
-      <li>
-        <CallListItem
-          call-id="H345"
-          call-priority="1"
-          call-priority-clr="priorityColor-green"
-          call-type-code="2319"
-          call-type-description="Human Contamination"
-          call-type-icon="bi bi-virus"
-          call-location="Scare Floor Sector 7G"
-          call-assigned-resources="P135, PS34, H43, H44"
-        />
-      </li>
-      <li>
-        <CallListItem
-          call-id="H345"
-          call-priority="1"
-          call-priority-clr="priorityColor-green"
-          call-type-code="2319"
-          call-type-description="Human Contamination"
-          call-type-icon="bi bi-virus"
-          call-location="Scare Floor Sector 7G"
-          call-assigned-resources="P135, PS34, H43, H44"
-        />
-      </li>
-      <li>
-        <CallListItem
-          call-id="H345"
-          call-priority="1"
-          call-priority-clr="priorityColor-green"
-          call-type-code="2319"
-          call-type-description="Human Contamination"
-          call-type-icon="bi bi-virus"
-          call-location="Scare Floor Sector 7G"
-          call-assigned-resources="P135, PS34, H43, H44"
-        />
-      </li>
-    </ul>
-  </div>
+  <ul
+    class="pf-v5-c-data-list pf-m-compact"
+    role="list"
+    aria-label="Simple data list example"
+    id="data-list-static-bottom-example-data-list"
+  >
+    <CallListItem
+      call-ID="H345"
+      call-priority="1"
+      call-priority-clr="green"
+      call-type-code="2319"
+      call-type-description="Human Contamination"
+      call-type-icon="bi bi-virus"
+      call-location="Scare Floor Sector 7G"
+      call-assigned-resources="P135, PS34, H43, H44"
+    />
+    <CallListItem
+      call-ID="H345"
+      call-priority="1"
+      call-priority-clr="green"
+      call-type-code="2319"
+      call-type-description="Human Contamination"
+      call-type-icon="bi bi-virus"
+      call-location="Scare Floor Sector 7G"
+      call-assigned-resources="P135, PS34, H43, H44"
+    />
+    <CallListItem
+      call-ID="H345"
+      call-priority="1"
+      call-priority-clr="green"
+      call-type-code="2319"
+      call-type-description="Human Contamination"
+      call-type-icon="bi bi-virus"
+      call-location="Scare Floor Sector 7G"
+      call-assigned-resources="P135, PS34, H43, H44"
+    />
+    <CallListItem
+      call-ID="H345"
+      call-priority="1"
+      call-priority-clr="green"
+      call-type-code="2319"
+      call-type-description="Human Contamination"
+      call-type-icon="bi bi-virus"
+      call-location="Scare Floor Sector 7G"
+      call-assigned-resources="P135, PS34, H43, H44"
+    />
+    <CallListItem
+      call-ID="H345"
+      call-priority="1"
+      call-priority-clr="green"
+      call-type-code="2319"
+      call-type-description="Human Contamination"
+      call-type-icon="bi bi-virus"
+      call-location="Scare Floor Sector 7G"
+      call-assigned-resources="P135, PS34, H43, H44"
+    />
+  </ul>
 </template>
-
-<style scoped></style>
