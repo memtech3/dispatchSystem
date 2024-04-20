a2b5449d0bbdf55afa552a248eaf61076560d70f
Micah Guttman <memtech3@gmail.com>
2024-04-20 07:51:22 +0000

    added update mutations

--- DispatchSystemBackend/GraphQLSchema/Mutation.cs
+++ DispatchSystemBackend/GraphQLSchema/Mutation.cs
@@ -5,11 +5,19 @@ namespace DispatchSystemBackend.GraphQLSchema
 {
     public class Mutation
     {
-
         public CadEventEntity CreateEvent(DispatchSystemBackendContext context, CadEventEntity inputEvent)
         {
             CadEventEntity cadEvent = inputEvent;
             context.CadEvents.Add(cadEvent);
+            context.SaveChanges();
+
+            return cadEvent;
+        }
+
+        public CadEventEntity UpdateEvent(DispatchSystemBackendContext context, int eventID, CadEventEntity inputEvent)
+        {
+            CadEventEntity cadEvent = context.CadEvents.FirstOrDefault(item => item.ID == eventID) ?? throw new Exception("CadEvent not found");
+            cadEvent.Name = inputEvent.Name;
             context.SaveChanges();
 
             return cadEvent;
@@ -24,10 +32,28 @@ namespace DispatchSystemBackend.GraphQLSchema
             return cadLogEntry;
         }
 
+        public CadLogEntryEntity UpdateLogEntry(DispatchSystemBackendContext context, int logEntryID, CadLogEntryEntity inputLogEntry)
+        {
+            CadLogEntryEntity cadLogEntry = context.CadLogEntries.FirstOrDefault(item => item.ID == logEntryID) ?? throw new Exception("CadLogEntry not found");
+            cadLogEntry.Name = inputLogEntry.Name;
+            context.SaveChanges();
+
+            return cadLogEntry;
+        }
+
         public UnitEntity CreateUnit(DispatchSystemBackendContext context, UnitEntity inputUnit)
         {
             UnitEntity unit = inputUnit;
             context.Units.Add(unit);
+            context.SaveChanges();
+
+            return unit;
+        }
+
+        public UnitEntity UpdateUnit(DispatchSystemBackendContext context, int unitID, UnitEntity inputUnit)
+        {
+            UnitEntity unit = context.Units.FirstOrDefault(item => item.ID == unitID) ?? throw new Exception("Unit not found");
+            unit.Name = inputUnit.Name;
             context.SaveChanges();
 
             return unit;
