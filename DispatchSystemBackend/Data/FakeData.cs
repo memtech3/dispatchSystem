79408c46c706afd5c04c8fe996bcc46a7507cda6
Micah Guttman <memtech3@gmail.com>
2024-04-27 01:44:55 +0000

    added seed so bogus produces the same data each time

--- DispatchSystemBackend/Data/FakeData.cs
+++ DispatchSystemBackend/Data/FakeData.cs
@@ -12,27 +12,29 @@ namespace DispatchSystemBackend.Data
 
         public static void Init(int count)
         {
+            int seed = 3660; // seed for Bogus's pseudo random generator, keeps output consistent
+
             int cadEventID = 1;
             Faker<CadEventEntity> cadEventFaker = new Faker<CadEventEntity>()
                 // FIXME: EF Core complains about duplicate IDs, what am I doing wrong?
                 .RuleFor(i => i.ID, _ => cadEventID++)
                 .RuleFor(i => i.Name, f => f.Hacker.Phrase());
-            cadEvents.AddRange(cadEventFaker.Generate(count));
+            cadEvents.AddRange(cadEventFaker.UseSeed(seed).Generate(count));
 
             int cadLogEntryID = 1;
             Faker<CadLogEntryEntity> cadLogEntryFaker = new Faker<CadLogEntryEntity>()
                 .RuleFor(i => i.ID, _ => cadLogEntryID++)
                 .RuleFor(i => i.Name, f => f.Hacker.Phrase());
-                // FIXME: CadEventEntries foreign key rules desn't work because faker returns CadLogEntity items 
-                // instead of a list of CadLogEntity IDs
-                // .RuleFor(i => i.CadEventEntries, f => f.Random.ListItems(cadEventEntities));
-            cadLogEntries.AddRange(cadLogEntryFaker.Generate(count));
+            // FIXME: CadEventEntries foreign key rules desn't work because faker returns CadLogEntity items 
+            // instead of a list of CadLogEntity IDs
+            // .RuleFor(i => i.CadEventEntries, f => f.Random.ListItems(cadEventEntities));
+            cadLogEntries.AddRange(cadLogEntryFaker.UseSeed(seed).Generate(count));
 
             int unitID = 1;
             Faker<UnitEntity> unitFaker = new Faker<UnitEntity>()
                 .RuleFor(i => i.ID, _ => unitID++)
                 .RuleFor(i => i.Name, f => f.Person.FullName);
-            units.AddRange(unitFaker.Generate(count));
+            units.AddRange(unitFaker.UseSeed(seed).Generate(count));
         }
     }
 }
