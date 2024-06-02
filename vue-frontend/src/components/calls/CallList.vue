61f03228cc3c80e4c8a955e68b73f0bd760ac31d
Micah Guttman <memtech3@gmail.com>
2024-06-02 07:43:50 +0000

    attempting to use graphql as source for CallList

--- vue-frontend/src/components/calls/CallList.vue
+++ vue-frontend/src/components/calls/CallList.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CallListItem from './CallListItem.vue'
+import { gql, useQuery } from '@urql/vue'
+
+const result = useQuery({
+  query: gql`
+    {
+      events {
+        id
+        location
+        cadLogEntries {
+          id
+          name
+        }
+        assignedUnits {
+          id
+          name
+        }
+      }
+    }
+  `
+})
+
+const reactiveResult = ref(result)
 </script>
 
 <template>
@@ -9,6 +32,29 @@ import CallListItem from './CallListItem.vue'
     aria-label="Simple data list example"
     id="data-list-static-bottom-example-data-list"
   >
+    <div v-if="reactiveResult.fetching">Loading...</div>
+    <div v-else-if="reactiveResult.error">Oh no... {{ reactiveResult.error }}</div>
+    <div v-else>
+      <ul v-if="reactiveResult.data.events">
+        <li
+          v-for="event in reactiveResult.data.events"
+          :key="event.id"
+          class="pf-v5-c-data-list__item"
+        >
+          <CallListItem
+            :call-ID="event.id"
+            call-priority="1"
+            call-priority-clr="green"
+            call-type-code="2319"
+            call-type-description=""
+            call-type-icon="bi bi-virus"
+            :call-location="event.location"
+            call-assigned-resources="P135, PS34, H43, H44"
+          />
+        </li>
+      </ul>
+    </div>
+    <!--
     <CallListItem
       call-ID="H345"
       call-priority="1"
@@ -18,46 +64,6 @@ import CallListItem from './CallListItem.vue'
       call-type-icon="bi bi-virus"
       call-location="Scare Floor Sector 7G"
       call-assigned-resources="P135, PS34, H43, H44"
-    />
-    <CallListItem
-      call-ID="H345"
-      call-priority="1"
-      call-priority-clr="green"
-      call-type-code="2319"
-      call-type-description="Human Contamination"
-      call-type-icon="bi bi-virus"
-      call-location="Scare Floor Sector 7G"
-      call-assigned-resources="P135, PS34, H43, H44"
-    />
-    <CallListItem
-      call-ID="H345"
-      call-priority="1"
-      call-priority-clr="green"
-      call-type-code="2319"
-      call-type-description="Human Contamination"
-      call-type-icon="bi bi-virus"
-      call-location="Scare Floor Sector 7G"
-      call-assigned-resources="P135, PS34, H43, H44"
-    />
-    <CallListItem
-      call-ID="H345"
-      call-priority="1"
-      call-priority-clr="green"
-      call-type-code="2319"
-      call-type-description="Human Contamination"
-      call-type-icon="bi bi-virus"
-      call-location="Scare Floor Sector 7G"
-      call-assigned-resources="P135, PS34, H43, H44"
-    />
-    <CallListItem
-      call-ID="H345"
-      call-priority="1"
-      call-priority-clr="green"
-      call-type-code="2319"
-      call-type-description="Human Contamination"
-      call-type-icon="bi bi-virus"
-      call-location="Scare Floor Sector 7G"
-      call-assigned-resources="P135, PS34, H43, H44"
-    />
+    /> -->
   </ul>
 </template>
--- vue-frontend/src/components/calls/CallListItem.vue
+++ vue-frontend/src/components/calls/CallListItem.vue
@@ -34,62 +34,55 @@ defineProps<{
 </script>
 
 <template>
-  <li
-    class="pf-v5-c-data-list__item"
-    aria-labelledby="data-list-static-bottom-example-data-list-item-1"
-  >
-    <div class="pf-v5-c-card pf-m-plain pf-m-compact">
-      <div class="pf-v5-l-grid pf-m-all-6-col-on-md">
-        <div class="pf-v5-l-grid__item pf-m-2-col" :class="'priorityColor-' + callPriorityClr">
-          <div class="pf-v5-l-stack pf-v5-u-p-sm">
-            <div class="pf-v5-l-stack__item pf-v5-u-m-auto">
-              <span class="fas fa-fw fa-info-circle"></span>
-            </div>
-            <div class="pf-v5-l-stack__item pf-m-fill pf-v5-u-m-auto">
-              <span class="pf-v5-u-font-size-3xl pf-v5-u-font-weight-bold"
-                >P{{ callPriority }}</span
-              >
-            </div>
-            <div class="pf-v5-l-stack__item">
-              <span class="pf-v5-u-font-size-xs">{{ callID }}</span>
-            </div>
+  <div class="pf-v5-c-card pf-m-plain pf-m-compact">
+    <div class="pf-v5-l-grid pf-m-all-6-col-on-md">
+      <div class="pf-v5-l-grid__item pf-m-2-col" :class="'priorityColor-' + callPriorityClr">
+        <div class="pf-v5-l-stack pf-v5-u-p-sm">
+          <div class="pf-v5-l-stack__item pf-v5-u-m-auto">
+            <span class="fas fa-fw fa-info-circle"></span>
+          </div>
+          <div class="pf-v5-l-stack__item pf-m-fill pf-v5-u-m-auto">
+            <span class="pf-v5-u-font-size-3xl pf-v5-u-font-weight-bold">P{{ callPriority }}</span>
+          </div>
+          <div class="pf-v5-l-stack__item">
+            <span class="pf-v5-u-font-size-xs">{{ callID }}</span>
           </div>
         </div>
-        <div class="pf-v5-l-grid__item pf-m-8-col">
-          <div class="pf-v5-l-stack pf-v5-u-p-sm">
-            <div class="pf-v5-l-stack__item">
-              <h2 class="pf-v5-c-content">{{ callTypeDescription }}</h2>
-            </div>
-            <div class="pf-v5-l-stack__item pf-m-fill">
-              {{ callLocation }}
-            </div>
-            <div class="pf-v5-l-stack__item">
-              <span class="pf-v5-c-label pf-m-blue pf-m-compact">
-                <span class="pf-v5-c-label__content">
-                  <span class="pf-v5-c-label__icon">
-                    <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
-                  </span>
-                  <span class="pf-v5-c-label__text" style="--pf-v5-c-label__text--MaxWidth: 38ch"
-                    >1A75</span
-                  >
+      </div>
+      <div class="pf-v5-l-grid__item pf-m-8-col">
+        <div class="pf-v5-l-stack pf-v5-u-p-sm">
+          <div class="pf-v5-l-stack__item">
+            <h2 class="pf-v5-c-content">{{ callTypeDescription }}</h2>
+          </div>
+          <div class="pf-v5-l-stack__item pf-m-fill">
+            {{ callLocation }}
+          </div>
+          <div class="pf-v5-l-stack__item">
+            <span class="pf-v5-c-label pf-m-blue pf-m-compact">
+              <span class="pf-v5-c-label__content">
+                <span class="pf-v5-c-label__icon">
+                  <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
                 </span>
+                <span class="pf-v5-c-label__text" style="--pf-v5-c-label__text--MaxWidth: 38ch"
+                  >1A75</span
+                >
               </span>
-              <span class="pf-v5-c-label pf-m-purple pf-m-compact">
-                <span class="pf-v5-c-label__content">
-                  <span class="pf-v5-c-label__icon">
-                    <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
-                  </span>
-                  <span class="pf-v5-c-label__text" style="--pf-v5-c-label__text--MaxWidth: 38ch"
-                    >ENG1</span
-                  >
+            </span>
+            <span class="pf-v5-c-label pf-m-purple pf-m-compact">
+              <span class="pf-v5-c-label__content">
+                <span class="pf-v5-c-label__icon">
+                  <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
                 </span>
+                <span class="pf-v5-c-label__text" style="--pf-v5-c-label__text--MaxWidth: 38ch"
+                  >ENG1</span
+                >
               </span>
-            </div>
+            </span>
           </div>
         </div>
       </div>
     </div>
-  </li>
+  </div>
 </template>
 
 <style lang="scss" scoped>
