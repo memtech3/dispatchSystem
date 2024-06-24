d5780676bdc45d99e6a9807f4ac8d1deb2588b6e
Micah Guttman <memtech3@gmail.com>
2024-06-23 19:32:35 -0500

    attempt at event window on f2

--- vue-frontend/src/views/DispatchView.vue
+++ vue-frontend/src/views/DispatchView.vue
@@ -1,8 +1,22 @@
-<script setup>
+<script setup lang="ts">
+import { createApp } from 'vue'
 import EventForm from '@/components/EventForm/EventForm.vue'
 import MapComponent from '@/components/MapComponent.vue'
 import UnitsTable from '@/components/UnitsTable.vue'
 import EventCardBoard from '@/components/events/EventCardBoard.vue'
+import { useMagicKeys, whenever } from '@vueuse/core'
+
+const keys = useMagicKeys()
+
+whenever(keys.F2, async () => {
+  // Dynamically import the NewEventWindow component
+  const { default: NewEventWindow } = await import('@/components/NewEventWindow.vue')
+  // Create an instance of the component
+  const NewEventWindowInstance = createApp(NewEventWindow)
+  // Mount the component to the body or a specific element
+  NewEventWindowInstance.mount("#windows")
+  console.log('Stuff')
+})
 </script>
 <template>
   <section class="pf-v5-c-page__main-section pf-v5-u-h-100">
@@ -21,4 +35,8 @@ import EventCardBoard from '@/components/events/EventCardBoard.vue'
       </div>
     </div>
   </section>
+
+  <div id="windows" v-for="number in numbers">
+
+  </div>
 </template>
