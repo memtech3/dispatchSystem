b9097604f2805ba873e840682b186d016fad4f7d
Micah Guttman <memtech3@gmail.com>
2024-06-23 21:11:13 -0500

    winbox opens on f2, does not start open

--- vue-frontend/src/views/DispatchView.vue
+++ vue-frontend/src/views/DispatchView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { createApp } from 'vue'
 import EventForm from '@/components/EventForm/EventForm.vue'
 import MapComponent from '@/components/MapComponent.vue'
 import UnitsTable from '@/components/UnitsTable.vue'
@@ -7,22 +6,29 @@ import EventCardBoard from '@/components/events/EventCardBoard.vue'
 import { useMagicKeys, whenever } from '@vueuse/core'
 
 import { ref } from 'vue'
-import { VueWinBox, useWinBox } from 'vue-winbox'
+import { VueWinBox } from 'vue-winbox'
 
 const wbRef = ref()
-const windowIsOpen = ref(true)
+const windowIsOpenRef = ref(false)
 
 //WinBox options
 const options = {
   title: 'New Event',
   overflow: true,
-  index: 2000
+  index: 2000,
+  x: 'center',
+  y: 'center'
 }
 
 const keys = useMagicKeys()
 
 whenever(keys.F2, () => {
+  if (windowIsOpenRef.value) {
+    wbRef.value.winbox.focus()
+  } else {
+    windowIsOpenRef.value = true
     wbRef.value?.initialize()
+  }
 })
 </script>
 <template>
@@ -43,10 +49,12 @@ whenever(keys.F2, () => {
     </div>
   </section>
 
-  <div id="windows">
+  <div id="windows" v-if="windowIsOpenRef">
     <VueWinBox
       ref="wbRef"
       :options="options"
+      @focus="windowIsOpenRef = true"
+      @close="windowIsOpenRef = false"
     >
       winbox window
     </VueWinBox>
