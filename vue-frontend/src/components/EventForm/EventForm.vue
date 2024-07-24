2990cfe25058100690e4bc89c64393e00dcb7bf4
Micah Guttman <memtech3@gmail.com>
2024-07-23 23:05:46 -0500

    put this in a card

--- vue-frontend/src/components/EventForm/EventForm.vue
+++ vue-frontend/src/components/EventForm/EventForm.vue
@@ -5,14 +5,26 @@ import CommentsPanel from './CommentsPanel.vue'
 </script>
 
 <template>
-  <form action="" class="row g-3">
-    <AutoCompleteDropdown class="col-md-12" id="location" label="Location" />
-    <AutoCompleteDropdown class="col-md-6" id="type" label="Call Type" />
-    <AutoCompleteDropdown class="col-md-6" id="subtype" label="Call Subtype" />
-    <InputText class="col-md-6" label="Call Source" id="callSource" />
-    <InputText class="col-md-6" label="Reporting Party" id="reportingParty" />
-    <InputText class="col-md-12" label="Callback Number" id="callBackNumber" />
-    <InputText class="col-md-12" label="Additional Persons" id="additionalPersons" />
-    <CommentsPanel />
-  </form>
+  <div class="card h-100" style="max-width: 540px">
+    <div class="row g-0 h-100">
+      <div class="col-md-1">
+        <!-- TODO: Navigation tabs go here -->
+      </div>
+      <div class="col-md-11 h-100">
+        <div class="card-body h-100 overflow-auto">
+          <h5 class="card-title">Card title</h5>
+          <form action="" class="row g-3">
+            <AutoCompleteDropdown class="col-md-12" id="location" label="Location" />
+            <AutoCompleteDropdown class="col-md-6" id="type" label="Call Type" />
+            <AutoCompleteDropdown class="col-md-6" id="subtype" label="Call Subtype" />
+            <InputText class="col-md-6" label="Call Source" id="callSource" />
+            <InputText class="col-md-6" label="Reporting Party" id="reportingParty" />
+            <InputText class="col-md-12" label="Callback Number" id="callBackNumber" />
+            <InputText class="col-md-12" label="Additional Persons" id="additionalPersons" />
+          </form>
+          <CommentsPanel />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
