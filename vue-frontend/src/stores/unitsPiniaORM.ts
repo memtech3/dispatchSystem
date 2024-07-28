d015469e278ffa2bc6574bb90a5640b4612f01f8
Micah Guttman <memtech3@gmail.com>
2024-07-27 23:03:32 -0500

    ran formatter

--- vue-frontend/src/stores/unitsPiniaORM.ts
+++ vue-frontend/src/stores/unitsPiniaORM.ts
@@ -11,5 +11,6 @@ export class UnitPiniaORM extends Model {
   @Str('') declare location: string
   @Str('') declare status: string
   @Attr(null) declare assignedEventId: string | null
-  @BelongsTo(() => CadEventPiniaORM, 'assignedEventId') declare assignedEvent: CadEventPiniaORM | null
+  @BelongsTo(() => CadEventPiniaORM, 'assignedEventId')
+  declare assignedEvent: CadEventPiniaORM | null
 }
