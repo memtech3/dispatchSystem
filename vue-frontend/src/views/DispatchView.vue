eb7ccea612b571454cfdd99f5e55f5d19084c2fc
Micah Guttman <memtech3@gmail.com>
2024-08-02 23:54:06 -0500

    removed padding on edges of page

--- vue-frontend/src/views/DispatchView.vue
+++ vue-frontend/src/views/DispatchView.vue
@@ -7,13 +7,13 @@ import NewEventWindow from '@/components/NewEventWindow.vue'
 </script>
 <template>
   <div class="row h-100">
-    <div class="col-3 h-100">
+    <div class="col-3 ps-0 h-100">
       <EventForm />
     </div>
     <div class="col h-100">
       <EventsBoard />
     </div>
-    <div class="col h-100">
+    <div class="col pe-0 h-100">
       <MapComponent class="h-50" />
       <UnitsTable class="h-50" />
     </div>
