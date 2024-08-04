8a993bf97bb541b73dbcda26faf2c099384c8a60
Micah Guttman <memtech3@gmail.com>
2024-08-03 23:36:36 -0500

    added command bar to app

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,16 +1,18 @@
 <script lang="ts" setup>
 import AppNavbar from '@/components/ApplicationFrame/AppNavbar.vue'
+import CommandBar from '@/components/ApplicationFrame/CommandBar.vue'
 </script>
 <template>
   <AppNavbar />
   <main class="container-fluid h-100 px-0" id="appMain">
     <RouterView />
+    <CommandBar />
   </main>
 </template>
 
 <style scoped>
 #appMain {
   padding-top: 2.5rem;
+  padding-bottom: 2.5rem;
 }
 </style>
-./components/ApplicationFrame/AppNavbar.vue
