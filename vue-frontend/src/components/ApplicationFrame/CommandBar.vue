2b10c950eb2a316914c52db9d59336df41e249bf
Micah Guttman <memtech3@gmail.com>
2024-08-04 17:52:42 -0500

    added contents to suggestions, only show on input focused

--- vue-frontend/src/components/ApplicationFrame/CommandBar.vue
+++ vue-frontend/src/components/ApplicationFrame/CommandBar.vue
@@ -6,6 +6,7 @@ import { onKeyStroke } from '@vueuse/core'
 const tokensArray = ref<string[]>([])
 const inputValue = ref('')
 const inputRef = ref(null)
+const inputFocused = ref(false)
 
 const addToken = () => {
   tokensArray.value.push(inputValue.value)
@@ -44,6 +45,50 @@ onKeyStroke(
 //   },
 //   { target: inputRef }
 // )
+
+type Argument = {
+  name: string
+  description: string
+  required: boolean
+}
+
+type Command = {
+  name: string
+  description: string
+  aliases: string[]
+  arguments: Argument[]
+}
+const commands: Command[] = [
+  {
+    name: 'New Event',
+    description: 'Creates a new event',
+    aliases: ['n', 'newevent'],
+    arguments: [
+      {
+        name: 'Location',
+        description: 'Location of event',
+        required: true
+      },
+      {
+        name: 'type',
+        description: 'Event type',
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'Select Event',
+    description: '',
+    aliases: ['s', 'selectevent'],
+    arguments: [
+      {
+        name: 'Event ID',
+        description: '',
+        required: true
+      }
+    ]
+  }
+]
 </script>
 
 <template>
@@ -52,13 +97,24 @@ onKeyStroke(
     <i class="bi bi-terminal pe-2"></i>
     <span class="text-secondary" v-for="token in tokensArray" :key="token">{{ token }}.</span>
     <div class="d-flex flex-fill">
-      <input class="flex-fill" ref="inputRef" type="text" v-model="inputValue" />
-      <ul class="list-group flex-column-reverse commandHints">
-        <li class="list-group-item active" aria-current="true">An active item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <input
+        class="flex-fill"
+        ref="inputRef"
+        type="text"
+        v-model="inputValue"
+        @focus="inputFocused = true"
+        @blur="inputFocused = false"
+      />
+      <ul
+        class="list-group commandHints"
+        :class="{ visible: inputFocused, invisible: !inputFocused }"
+      >
+        <li class="list-group-item p-1" v-for="command in commands" :key="command.name">
+          <p class="m-0">{{ command.name }}, {{ command.aliases }}</p>
+          <small>
+            <span v-for="arg in command.arguments" :key="arg.name">{{ arg.name }}, </span>
+          </small>
+        </li>
       </ul>
     </div>
   </div>
