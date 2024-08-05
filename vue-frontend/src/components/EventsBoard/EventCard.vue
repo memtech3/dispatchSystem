d2282d6b8605558871954364d49746e247396d34
Micah Guttman <memtech3@gmail.com>
2024-08-05 01:41:24 -0500

    better layout

--- vue-frontend/src/components/EventsBoard/EventCard.vue
+++ vue-frontend/src/components/EventsBoard/EventCard.vue
@@ -11,32 +11,35 @@ defineProps<{
   typeIcon: string
   location: string
   assignedUnits: UnitEntity[]
-  createdTime: string
+  ceatedTime: string
 }>()
 </script>
 
 <template>
   <div class="list-group-item list-group-item-action eventItemGrid p-0" aria-current="true">
-    <div class="triagePane verticalStack" :class="'priorityColor-' + priorityClr">
-      <span>type icon</span>
-      <span>P{{ priority }}</span>
-      <span>{{ id }}</span>
+    <div class="triagePane d-flex flex-column" :class="'priorityColor-' + priorityClr">
+      <div class="box m-1 bg-body-secondary d-flex align-items-center justify-content-center">
+        <span class="bi bi-exclamation-triangle"></span>
+      </div>
+      <span class="fs-2 text-center">P{{ priority }}</span>
     </div>
-    <span class="type fs-5 text-uppercase">{{ typeDescription }}</span>
-    <span class="subtype text-capitalize">subtype</span>
-    <span class="location text-capitalize">{{ location }}</span>
-    <div class="resources d-inline-flex gap-1 px-1">
-      <small v-for="unit in assignedUnits" :key="unit.id">
-        <span class="badge rounded-pill text-bg-primary">{{ unit.callsign }}</span>
-      </small>
+    <div class="detailsPane d-flex flex-column">
+      <span class="fs-5 text-uppercase">{{ typeDescription }}</span>
+      <span class="text-capitalize">subtype</span>
+      <span class="text-capitalize">{{ location }}</span>
+      <small>{{ id }}</small>
+      <div class="d-inline-flex gap-1 px-1 pb-1">
+        <small v-for="unit in assignedUnits" :key="unit.id">
+          <span class="badge rounded-pill text-bg-primary">{{ unit.callsign }}</span>
+        </small>
+      </div>
     </div>
-    <div class="timePane verticalStack">
+    <div class="timePane d-flex flex-column">
       <small>C 5:32</small>
       <small>P 2:21</small>
       <small>A 3:11</small>
       <small>M 10:00</small>
     </div>
-    <div class="actionsPane verticalStack">A</div>
   </div>
 </template>
 
@@ -57,40 +60,28 @@ defineProps<{
 
 .eventItemGrid {
   display: grid;
-  grid-template-columns: 0.25fr 1fr 0.25fr 0.125fr;
+  grid-template-columns: 0.25fr 1fr 0.3fr;
   grid-template-rows: repeat(4, 1fr);
   grid-column-gap: 5px;
   grid-row-gap: 0px;
   grid-template-areas:
-    'triagePane type timePane actionsPane'
-    'triagePane subtype timePane actionsPane'
-    'triagePane location timePane actionsPane'
-    'triagePane resources timePane actionsPane';
-}
-.verticalStack {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+    'triagePane detailsPane timePane'
+    'triagePane detailsPane timePane'
+    'triagePane detailsPane timePane'
+    'triagePane detailsPane timePane';
 }
 .triagePane {
   grid-area: triagePane;
 }
-.type {
-  grid-area: type;
-}
-.subtype {
-  grid-area: subtype;
-}
-.location {
-  grid-area: location;
-}
-.resources {
-  grid-area: resources;
+.detailsPane {
+  grid-area: detailsPane;
 }
 .timePane {
   grid-area: timePane;
 }
-.actionsPane {
-  grid-area: actionsPane;
+.box {
+  aspect-ratio: 1 / 1;
+  mask-image: url('@/assets/diamond-mask.svg');
+  mask-repeat: no-repeat;
 }
 </style>
