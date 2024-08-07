a49d6b710e1a7e54f2d6e77c62814bb053d6d4ef
Micah Guttman <memtech3@gmail.com>
2024-08-06 22:47:56 -0500

    reworked command hinting system

--- vue-frontend/src/components/ApplicationFrame/CommandBar.vue
+++ vue-frontend/src/components/ApplicationFrame/CommandBar.vue
@@ -2,43 +2,14 @@
 import { computed, ref } from 'vue'
 import { onKeyStroke } from '@vueuse/core'
 
-const tokensArray = ref<string[]>([])
 const inputValue = ref('')
 const inputRef = ref<HTMLElement | null>(null)
 const inputFocused = ref(false)
 
-const addToken = () => {
-  tokensArray.value.push(inputValue.value)
-  inputValue.value = ''
-}
-
 const runCommand = () => {
   console.log('Running command:', tokensArray.value)
   inputValue.value = ''
-  tokensArray.value = []
 }
-
-// add token to tokensArray when "." key is pressed
-onKeyStroke(
-  '.',
-  (e) => {
-    e.preventDefault()
-    addToken()
-  },
-  { target: inputRef }
-)
-
-// shift input to previous token when input is empty and backspace key is pressed
-onKeyStroke(
-  'Backspace',
-  (e) => {
-    if (inputValue.value === '') {
-      e.preventDefault()
-      inputValue.value = tokensArray.value.pop() ?? ''
-    }
-  },
-  { target: inputRef }
-)
 
 // TODO: figure out why this doesn't fire on shift+backspace
 // delete current token when shift and backspace key are pressed together
@@ -101,7 +72,8 @@ const commands: Command[] = [
         position: 1
       }
     ]
-  },  {
+  },
+  {
     name: 'New Field Initiated Event',
     description: '',
     aliases: ['nfe', 'newfieldevent'],
@@ -116,17 +88,21 @@ const commands: Command[] = [
   }
 ]
 
+const tokensArray = computed(() => {
+  return inputValue.value.split('.')
+})
+
 const filteredCommands = computed(() => {
-  if (tokensArray.value.length === 0) {
+  if (tokensArray.value.length < 2) {
     return commands.filter((command) => {
       return command.aliases.some((alias) =>
-        alias.toLowerCase().startsWith((inputValue.value ?? '').toLowerCase())
+        alias.toLowerCase().startsWith((tokensArray.value[0] ?? '').toLowerCase())
       )
     })
   } else {
     return commands.filter((command) => {
       return command.aliases.some(
-        (alias) => alias.toLowerCase() === (tokensArray.value.at(0) ?? '').toLowerCase()
+        (alias) => alias.toLowerCase() === (tokensArray.value[0] ?? '').toLowerCase()
       )
     })
   }
@@ -136,7 +112,6 @@ const filteredCommands = computed(() => {
 <template>
   <div class="commandBar form-control position-relative d-flex align-items-center">
     <i class="bi bi-terminal pe-2"></i>
-    <span class="text-secondary" v-for="token in tokensArray" :key="token">{{ token }}.</span>
     <div class="d-flex flex-fill">
       <input
         class="flex-fill p-0"
@@ -146,7 +121,7 @@ const filteredCommands = computed(() => {
         @focus="inputFocused = true"
         @blur="inputFocused = false"
         @keydown.enter="runCommand"
-        />
+      />
       <ul
         class="list-group commandHints"
         :class="{ visible: inputFocused, invisible: !inputFocused }"
@@ -161,7 +136,7 @@ const filteredCommands = computed(() => {
             <span
               v-for="arg in command.arguments"
               :key="arg.name"
-              :class="{ 'bg-primary': arg.position == tokensArray.length }"
+              :class="{ 'bg-primary': arg.position == tokensArray.length - 1 }"
               >{{ arg.name }},
             </span>
           </small>
