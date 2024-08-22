ffaf724b00498eb77ed316e45b2a5db4b54853a8
Micah Guttman <memtech3@gmail.com>
2024-08-22 12:57:22 -0500

    added pinia-shared-state to share state between windows

--- vue-frontend/src/main.ts
+++ vue-frontend/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createORM } from 'pinia-orm'
 
+import { PiniaSharedState } from 'pinia-shared-state'
+
 import App from './App.vue'
 import router from './router'
 
@@ -11,7 +13,11 @@ import urql, { cacheExchange, fetchExchange } from '@urql/vue'
 
 const app = createApp(App)
 
-app.use(createPinia().use(createORM()))
+app.use(
+  createPinia()
+    .use(createORM())
+    .use(PiniaSharedState({ enable: true }))
+)
 app.use(router)
 
 app.use(urql, {
