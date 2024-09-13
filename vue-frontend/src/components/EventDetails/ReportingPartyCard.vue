6a11fc6dcd2839ed15d018f92110ae2149ebc3aa
Micah Guttman <memtech3@gmail.com>
2024-09-13 01:22:30 -0500

    text should have been full opacity

--- vue-frontend/src/components/EventDetails/ReportingPartyCard.vue
+++ vue-frontend/src/components/EventDetails/ReportingPartyCard.vue
@@ -9,13 +9,13 @@ defineProps<{
   <div class="list-group-item list-group-item-action d-flex px-2 py-1 gap-2">
     <div class="w-100">
       <div class="d-flex flex-row align-items-center w-100">
-        <span class="fw-bold opacity-50 flex-grow-1"
+        <span class="fw-bold flex-grow-1"
           >{{ reportingParty.lastName }}, {{ reportingParty.firstName }}
           {{ reportingParty.middleName }}
       </span>
-        <span class="fw-bold opacity-50">{{ reportingParty.howReported }}</span>
+        <span class="fw-bold">{{ reportingParty.howReported }}</span>
       </div>
-      <p class="mb-0 opacity-75">
+      <p class="mb-0">
         Callback Phone: {{ reportingParty.callbackPhone }} <br />
         From Phone: {{ reportingParty.fromPhone }} <br />
         Location: {{ reportingParty.reportingPartyLocation }}
