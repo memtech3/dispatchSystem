3092aa27c6970405ed1458f8ba484493b9e7dd0a
Micah Guttman <memtech3@gmail.com>
2024-10-01 13:47:36 -0500

    removed garbage lines

--- vue-frontend/src/views/ConfigurationView.vue
+++ vue-frontend/src/views/ConfigurationView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import InputText from '@/components/Common/InputText.vue'
 import { useSystemConfigStore } from '@/stores/systemConfig'
 import EditableTable from '@/components/Common/EditableTable.vue'
 
@@ -7,12 +6,6 @@ const systemConfigStore = useSystemConfigStore()
 </script>
 <template>
   <div>
-    <h1>Configuration</h1>
-    <p>This is the configuration view. Things that belong here:</p>
-    <ul>
-      <li>Editable list of event types</li>
-      <li>Editable list of how reported options</li>
-    </ul>
     <div class="row m-5">
       <EditableTable
         id="eventTypes"
