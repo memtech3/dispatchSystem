cfc40d988477b90adab2b1ff38b63068ae7e640e
Micah Guttman <memtech3@gmail.com>
2024-10-01 13:58:12 -0500

    fixed violation of "don't mutate props" rule

--- vue-frontend/src/components/Common/EditableTable.vue
+++ vue-frontend/src/components/Common/EditableTable.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 import InputText from '@/components/Common/InputText.vue' // Import the InputText component
 import 'bootstrap-icons/font/bootstrap-icons.css'
-const model = defineModel()
 interface Column {
   key: string
   label: string
@@ -12,15 +11,32 @@ interface Row {
   id: number
 }
 
-defineProps<{
+const props = defineProps<{
   id: string
   columns: Column[]
   rows: Row[]
 }>()
+
+// TODO: make sure I'm using events properly. Currently
+// the rows are updated in the parent component even though
+// I haven't implemented an event handler in the parent
+const emit = defineEmits<{
+  (e: 'rowsUpdated', value: Row[]): void
+}>()
+
+const editedRows = computed({
+  get() {
+    return props.rows
+  },
+
+  set(newValues: Row[]) {
+    emit('rowsUpdated', newValues)
+  }
+})
 </script>
 <template>
   <div class="col-3">
-    <button @click="rows.push({})" class="btn btn-primary btn-sm">Add</button>
+    <button @click="editedRows.push({})" class="btn btn-primary btn-sm">Add</button>
     <table class="table table-sm">
       <thead>
         <tr>
@@ -30,7 +46,7 @@ defineProps<{
         </tr>
       </thead>
       <tbody>
-        <tr v-for="row in rows" :key="row.id">
+        <tr v-for="row in editedRows" :key="row.id">
           <td>
             <input type="checkbox" />
           </td>
@@ -43,7 +59,7 @@ defineProps<{
                 <i class="bi bi-pencil-square"></i>
               </button>
               <button
-                @click="rows.splice(rows.indexOf(row), 1)"
+                @click="editedRows.splice(editedRows.indexOf(row), 1)"
                 class="btn btn-sm btn-outline-danger"
               >
                 <i class="bi bi-trash"></i>
