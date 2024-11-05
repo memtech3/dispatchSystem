967cbf515ae4e140b27ab16d108785d2da2727ad
Micah Guttman <memtech3@gmail.com>
2024-11-04 20:05:19 -0600

    proof of concept use of dockvue library

---
+++ vue-frontend/src/DockView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { DockviewVue } from 'dockview-vue'
+
+import type { DockviewReadyEvent } from 'dockview-vue'
+import CommandBar from '@/components/ApplicationFrame/CommandBar/CommandBar.vue'
+import StatusBar from '@/components/ApplicationFrame/StatusBar.vue'
+
+const onReady = (event: DockviewReadyEvent) => {
+  const panel = event.api.addPanel({
+    id: 'panel_1',
+    component: 'UnitsTable',
+    title: 'Units Table'
+  })
+
+  event.api.addPanel({
+    id: 'panel_2',
+    component: 'EventsBoard',
+    title: 'Panel 2',
+    position: { referencePanel: panel }
+  })
+
+  const panel3 = event.api.addPanel({
+    id: 'panel_3',
+    component: 'MapComponent',
+    title: 'Panel 3',
+    position: { referencePanel: panel, direction: 'right' }
+  })
+
+  event.api.addPanel({
+    id: 'panel_4',
+    component: 'AddSeedDataButton',
+    title: 'Panel 4',
+    position: { referencePanel: panel3 }
+  })
+}
+</script>
+<template>
+  <DockviewVue style="width: 100%; height: 100%" class="dockview-theme-abyss" @ready="onReady" />
+  <CommandBar id="appCommandBar" />
+  <StatusBar />
+</template>
--- vue-frontend/src/main.ts
+++ vue-frontend/src/main.ts
@@ -10,10 +10,17 @@ import router from './router'
 import '@/main.scss'
 import 'fundamental-styles/dist/theming/sap_horizon_dark.css'
 import '@sap-theming/theming-base-content/content/Base/baseLib/sap_horizon_dark/css_variables.css'
+import 'dockview-vue/dist/styles/dockview.css'
 
 import urql, { cacheExchange, fetchExchange } from '@urql/vue'
 
-const app = createApp(App)
+import DockView from './DockView.vue'
+import UnitsTable from './components/UnitsTable.vue'
+import EventsBoard from './components/EventsBoard/EventsBoard.vue'
+import MapComponent from './components/MapComponent.vue'
+import AddSeedDataButton from './components/AddSeedDataButton.vue'
+
+const app = createApp(DockView)
 
 app.use(
   createPinia()
@@ -26,5 +33,11 @@ app.use(urql, {
   url: 'http://localhost:5107/graphql',
   exchanges: [cacheExchange, fetchExchange]
 })
+
+// Register components here so dockview can access them by name
+app.component('UnitsTable', UnitsTable)
+app.component('EventsBoard', EventsBoard)
+app.component('MapComponent', MapComponent)
+app.component('AddSeedDataButton', AddSeedDataButton)
 
 app.mount('#app')
