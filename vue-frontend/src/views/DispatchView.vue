17ff8c01ccc9d60dd086a46223a38676b8508083
Micah Guttman <memtech3@gmail.com>
2024-12-18 23:51:03 -0700

    first pass at load layout dialog

--- vue-frontend/src/views/DispatchView.vue
+++ vue-frontend/src/views/DispatchView.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import NewEventWindow from '@/components/NewEventWindow.vue'
 import { DockviewVue } from 'dockview-vue'
 
 import Toolbar from '@/components/Common/Toolbar/Toolbar.vue'
+
+import 'fundamental-styles/dist/list.css'
+import 'fundamental-styles/dist/icon.css'
 
 import type { DockviewReadyEvent } from 'dockview-vue'
 import ToolbarMenu from '@/components/Common/Toolbar/ToolbarMenu.vue'
 import ToolbarMenuItem from '@/components/Common/Toolbar/ToolbarMenuItem.vue'
 import ToolbarMenuSubMenu from '@/components/Common/Toolbar/ToolbarMenuSubMenu.vue'
 import ToolbarButton from '@/components/Common/Toolbar/ToolbarButton.vue'
+import DialogComponent from '@/components/Common/Dialog/DialogComponent.vue'
+import DialogHeader from '@/components/Common/Dialog/DialogHeader.vue'
+import DialogFooter from '@/components/Common/Dialog/DialogFooter.vue'
+import DialogFooterButton from '@/components/Common/Dialog/DialogFooterButton.vue'
+
+import { useSystemConfigStore } from '@/stores/systemConfig'
+import { useUserConfigStore } from '@/stores/userConfig'
+import DialogSubheader from '@/components/Common/Dialog/DialogSubheader.vue'
+
+const systemConfigStore = useSystemConfigStore()
+const userConfigStore = useUserConfigStore()
 
 let dockviewAPI: DockviewReadyEvent['api']
 
@@ -24,12 +38,13 @@ const saveLayout = () => {
   }
 }
 
-const loadLayout = () => {
-  const savedLayout = localStorage.getItem('dockviewLayout')
+function loadLayout(id: number) {
+  const savedLayout = userConfigStore.savedLayouts.find((layout) => layout.id === id)
   if (savedLayout && dockviewAPI) {
-    let layout = JSON.parse(savedLayout)
+    let layout = savedLayout.layoutObject
     dockviewAPI.fromJSON(layout)
   }
+  loadLayoutDialog.value.hide()
 }
 
 function addPanel(component: string, title: string): string {
@@ -59,6 +74,9 @@ onMounted(() => {
     }
   })
 })
+
+const loadLayoutDialog = ref()
+const selectedLayoutID = ref() // to be used by the load/manage layout dialogs only
 </script>
 <template>
   <div id="viewContainer">
@@ -66,7 +84,7 @@ onMounted(() => {
       <ToolbarMenu>
         <template #button-text> Layout </template>
         <template #menu-items>
-          <ToolbarMenuItem @click="loadLayout">Load Layout</ToolbarMenuItem>
+          <ToolbarMenuItem @click="loadLayoutDialog.show">Load Layout</ToolbarMenuItem>
           <ToolbarMenuItem @click="saveLayout">Save Layout</ToolbarMenuItem>
           <ToolbarMenuSubMenu>
             <template #button-text>Add Panel</template>
@@ -99,6 +117,77 @@ onMounted(() => {
     />
   </div>
   <NewEventWindow />
+
+  <DialogComponent ref="loadLayoutDialog" @focusout="selectedLayoutID = ''">
+    <template #header>
+      <DialogHeader class="fd-bar--header-with-subheader">
+        <template #title>Load Layout</template>
+      </DialogHeader>
+    </template>
+    <template #subheader>
+      <DialogSubheader
+        ><div class="fd-bar__middle">
+          <div class="fd-input-group">
+            <input
+              class="fd-input fd-input-group__input"
+              type="text"
+              aria-label="search"
+              placeholder="Search..."
+            />
+            <span class="fd-input-group__addon fd-input-group__addon--button">
+              <button
+                class="fd-button fd-input-group__button fd-button--icon fd-button--transparent"
+                aria-label="perform search"
+              >
+                <i class="sap-icon--search" role="presentation"></i>
+              </button>
+            </span>
+          </div>
+        </div>
+      </DialogSubheader>
+    </template>
+    <template #body>
+      <ul v-if="userConfigStore.savedLayouts.length != 0" class="fd-list" role="list">
+        <li
+          class="fd-list__item fd-list__item--interractive"
+          :class="{ 'is-selected': selectedLayoutID === layout.id }"
+          role="listitem"
+          v-for="layout in userConfigStore.savedLayouts"
+          :key="layout.id"
+          @click="selectedLayoutID = layout.id"
+          @dblclick="loadLayout(layout.id)"
+        >
+          <span class="fd-list__title">{{ layout.name }}</span>
+        </li>
+      </ul>
+      <div v-else class="fd-message-page">
+        <div class="fd-message-page__container">
+          <div class="fd-message-page__icon-container">
+            <i role="presentation" class="sap-icon--product fd-message-page__icon"></i>
+          </div>
+          <div role="status" aria-live="polite" class="fd-message-page__content">
+            <div class="fd-message-page__title">No items are currently available.</div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <DialogFooter>
+        <template #right>
+          <DialogFooterButton
+            @click="loadLayout(selectedLayoutID)"
+            button-class="fd-button--emphasized"
+          >
+            Load
+          </DialogFooterButton>
+          <DialogFooterButton @click="loadLayoutDialog.hide" button-class="fd-button--transparent">
+            Cancel
+          </DialogFooterButton>
+        </template>
+      </DialogFooter>
+    </template>
+  </DialogComponent>
+  <!-- Manage layout dialog go here -->
 </template>
 <style lang="scss">
 #viewContainer {
